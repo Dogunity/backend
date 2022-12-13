24036b1c360c0a77be54def26ddcf31caf609b25
robinyeon <robin.yeon@gmail.com>
2022-12-14 00:30:37 +0900

    FIX: mypage_owning community list

--- src/controllers/user.ctrl.js
+++ src/controllers/user.ctrl.js
@@ -53,20 +53,15 @@ export default {
   },
 
   async getMyCommunity(req, res, next) {
-    const { page } = req.query;
     const userId = req.userId;
     try {
-      const totalMyCommunityPage = await userService.countMyCommunityPage(
-        page,
-        userId,
-      );
       const myCommunities = await userService.getMyCommunities(userId);
 
       return res.status(200).json({
         success: true,
         status: 200,
         message: 'Successfully GET my community list.',
-        result: { totalMyCommunityPage, myCommunities },
+        result: myCommunities,
       });
     } catch (err) {
       next(err);
